b690cd419f318108c17d1a2bcc2ce896cd736363
Richard Bettridge <r.bettridge@vehikl.com>
2018-03-02 11:02:19 -0500

    removed generic function version, class only going forward

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ For those who want to support multiple languages without a lot of complication, 
 - Easily override default language
 - Supports static and dynamic strings (can pass variables into text)
 - Decent error handling
-- Can be imported as a class or a generic function
+
 
 ### Structure of a language file
 ```
@@ -26,18 +26,18 @@ export default {
 ```
 Simply populate your translations in the messages object seen in the example above and save the file somewhere in your project, such as in ./translations/english.js
 
-## Class Usage
+## Usage
 
-see *./examples/index-class.html*
+see *./examples/index.html*
 
 ### Getting Started
 
-- import the simple-translation-class.js file
+- import the simple-translation.js file
 - import your supplied translation files
 - register each language
 - call the message() method to get a translated string
 ```
-    import translate from '../simple-translation-class.js'
+    import translate from '../simple-translation.js'
 
     import english from '../languages/english.js'
     import french from '../languages/french.js'
@@ -84,31 +84,3 @@ returns: ```{
     exampleFunction: variable => `Example function that returns a string plus a variable of ${variable}`,
   },
 }```
-
-
-
-## Generic Function Usage
-
-Default import usage with browser language auto detection. Language file imports are hardcoded in simple-translation.js. This is typically not a good practice but I think this is easier to follow for less seasoned people.
-
-see *./examples/index.html*
-
-```
-import translate from 'simple-translation'
-
-translate.messages.exampleString //renders a string
-
-translate.messages.exampleFunction('John')
-
-```
-
-
-Explicitly setting which translation file to use
-
-```
-
-  import { getLocale } from '../simple-translation.js'
-
-  getLocale('fr').messages.exampleString
-
-```
--- simple-translation.js
+++ simple-translation.js
@@ -1,16 +1,40 @@
-import en from "./languages/english.js"
-import fr from "./languages/french.js"
+export default class Translate{
+  constructor(...languageFiles){
+    this.localeData = {}
+    this.browserLanguageCode = (
+      typeof navigator === "undefined"
+      ? "en"
+      : (navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage
+    ).toLowerCase().split(/[_-]+/)[0]
+    languageFiles.forEach((language) => {
+      this.registerLanguage(language)
+    })
+  }
 
-const localeData = { en, fr }
+  registerLanguage(languageFile){
+    if (!languageFile){
+      console.error(`Simple-Translation: Language File '${languageFile.language}' not found`)
+      return
+    }
+    if (this.localeData[languageFile.languageCode]){
+      console.error(`Simple-Translation: Language File '${languageFile.language}' already registered`)
+      return
+    }
+    this.localeData[languageFile.languageCode] = languageFile
+  }
 
-const language = (navigator.languages && navigator.languages[0]) ||
-                  navigator.language ||
-                  navigator.userLanguage
+  getLocale(laguageCode){
+    if (!this.localeData[laguageCode]){
+      console.error(`Simple-Translation: Language definition of '${laguageCode}' Not Found`)
+    }
+    return this.localeData[laguageCode]
+  }
 
-const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0]
-
-export default (localeData[languageWithoutRegionCode] || localeData[language] || localeData.en)
-
-export const getLocale = (lang) => {
-  return localeData[lang]
+  message(key, languageCode = this.browserLanguageCode){
+    if (!this.localeData[languageCode].messages[key]){
+      console.error(`Simple-Translation: The message '${key}' for language code '${languageCode}' was not found`)
+      return '<span style="color:red;">Missing Translation</span>'
+    }
+    return this.localeData[languageCode].messages[key]
+  }
 }
